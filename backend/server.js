6bf8755a5f38a59f895375df003ca533b5d6f3c3
MamaWin01 <marcellinton0852@gmail.com>
2024-04-26 15:12:19 +0700

    first push

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const mysql   = require('mysql')
+const cors    = require('cors')
+
+const app = express()
+app.use(cors())
+
+app.get('/', (re, res) => {
+    return res.json("test");
+})
+
+app.listen(8001, () => {
+    console.log('localhost:8001');
+})
